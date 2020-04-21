f514a51b914205dd8e98f415240a6b162c24f678
Thomas <thomas.legge.419@gmail.com>
2020-04-22 00:03:45 +1200

    Initial Commit

---
+++ GoodMorning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoodMorning
+//
+//  Created by Thomas Legge on 22/04/20.
+//  Copyright © 2020 Thomas Legge. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
